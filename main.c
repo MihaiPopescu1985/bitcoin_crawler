e58a736d9beb8c5d166a5f25afb2b9b0aa7f9cfa
Mihai Popescu <34679869+MihaiPopescu1985@users.noreply.github.com>
2024-09-24 18:46:36 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,34 +1,16 @@
 #include "blk_dat_parser.h"
-#include <sqlite3.h>
 
 int main(int argc, char **argv) {
-    // Database
-    sqlite3 *db;
-    if (sqlite3_open("../db/bitcoin.db", &db))
-    {
-        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
-        sqlite3_close(db);
-        return(1);
-    }
-
     // Read the file
     FILE *dat_file = fopen(argv[2], "rb");
     if (dat_file == NULL) {
-        perror("dat file could not be opened\n");
-        sqlite3_close(db);
+        fprintf(stderr, "Dat file could not be opened\n");
         return 1;
     }
 
-    // Parse the file
-    printf("Start parsing %s\n", argv[2]);
-    char *log_level = (argc < 4) ? "none" : argv[3];
+    int result = parse_dat_file(dat_file);
+    fclose(dat_file);
 
-    int result = parse_dat_file(dat_file, db, log_level);
-
-    // Conclude
-    sqlite3_close(db);
-    if(result == 0) printf("Parsing was successful.\n");
-    else printf("Parsing failed. Error code %d\n", result);
-
+    if(result != 0) fprintf(stderr, "Parsing failed. Error code %d\n", result);
     return result;
 }
