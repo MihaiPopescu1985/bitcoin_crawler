4d4769e8948502f41458cf2126333457092cbd20
Mihai Popescu <34679869+MihaiPopescu1985@users.noreply.github.com>
2024-09-24 18:47:47 +0300

    Update blk_dat_parser.h

--- blk_dat_parser.h
+++ blk_dat_parser.h
@@ -1,9 +1,8 @@
 #ifndef BLK_DAT_PARSER
 #define BLK_DAT_PARSER
 
-#include <sqlite3.h>
 #include <stdio.h>
 
-int parse_dat_file(FILE *dat_file, sqlite3 *database, char *log_level);
+int parse_dat_file(FILE *dat_file);
 
 #endif
